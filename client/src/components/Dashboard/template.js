080d443c4b4626c5530478c505bcbf2e187ea2eb
unknown <jiayi1172@gmail.com>
2019-10-07 00:16:17 +0800

    Added resume templates

--- client/src/components/Dashboard/template.js
+++ client/src/components/Dashboard/template.js
@@ -3,6 +3,7 @@ import NavBar from './navbar';
 import { Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button, Container, Row, Col, CardImgOverlay } from 'reactstrap';
 import "./dashboard.css"
+import {templates} from '../../resume-templates/template'
 
 export default class Template extends React.Component {
   constructor(props) {
@@ -10,34 +11,30 @@ export default class Template extends React.Component {
   } 
 
   render() {  
+    console.log(templates)
     return (
       <div>
           <Container>
             <Row>
-              <Col md="4">
-              <Card inverse className="cards">
-                <CardImg width="100%" src="https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Card image cap" />
-                <CardImgOverlay>
-                  <CardTitle>Card Title</CardTitle>
-                  <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                  <CardText>
-                    <small className="text-muted">Last updated 3 mins ago</small>
-                  </CardText>
-                </CardImgOverlay>
-              </Card>
-              </Col>
-              <Col md="4">
-              <Card inverse className="cards">
-                <CardImg width="100%" src="https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Card image cap" />
-                <CardImgOverlay>
-                  <CardTitle>Card Title</CardTitle>
-                  <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                  <CardText>
-                    <small className="text-muted">Last updated 3 mins ago</small>
-                  </CardText>
-                </CardImgOverlay>
-              </Card>
-              </Col>
+              {templates.map((template, key) => {
+                return <Col md="4" key={template.id}>
+                <Card inverse className="cards" className="imgOverlay">
+                  <CardImg width="100%" src={template.img} alt={`${template.name} template`} />
+                  <CardImgOverlay>
+                    <div className="textOverlay">
+                      <CardTitle>{template.name}</CardTitle>
+                      <Button color="primary">SELECT</Button>
+                    </div>
+                    {/* <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+                    <CardText>  */}
+                      {/* <small className="text-muted">Last updated 3 mins ago</small>
+                    </CardText> */}
+                  </CardImgOverlay>
+                  {/* <CardTitle>{template.name}</CardTitle> */}
+                </Card>
+                </Col>
+              })}
+              
             </Row>
           </Container>
       </div>
---
+++ client/src/resume-templates/template.js
@@ -0,0 +1,15 @@
+import template1 from '../images/aresume-template.png';
+import template2 from '../images/aresume-template2.png';
+
+export const templates = [
+    {
+        id: 1,
+        img: template1,
+        name: "Metallic Gold"
+    },
+    {
+        id: 2,
+        img: template2,
+        name: "Pink Blossom"
+    }
+]
