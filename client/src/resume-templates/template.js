d25fbfee7e06f3844dfa7e5f6e6eb8d57ff66e95
unknown <jiayi1172@gmail.com>
2019-10-08 22:56:32 +0800

    Added template background

--- client/src/resume-templates/template.js
+++ client/src/resume-templates/template.js
@@ -1,15 +1,19 @@
 import template1 from '../images/aresume-template.png';
 import template2 from '../images/aresume-template2.png';
+import background1 from '../images/aresume-template-background.png';
+import background2 from '../images/aresume-template2-background.png';
 
 export const templates = [
     {
         id: 1,
         img: template1,
-        name: "Metallic Gold"
+        name: "Metallic Gold",
+        backgroundImage: background1
     },
     {
         id: 2,
         img: template2,
-        name: "Pink Blossom"
+        name: "Pink Blossom",
+        backgroundImage: background2
     }
 ]
